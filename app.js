c0d2b1e8c44b963789ab50f8f249c7b81703f89f
Antriksh Yadav <antrikshy@gmail.com>
2015-05-23 18:25:33 -0700

    Fixed blank names problem

--- app.js
+++ app.js
@@ -73,14 +73,9 @@ numOfUsers = 0;
 var messageColors = ["#FFFFFF", "#044B7F"];
 io.on('connection', function (socket) {
     var userIp = socket.client.request.headers['x-forwarded-for'];
-    
-    if (!socket.handshake.query.tag) {
-        socket.disconnect();
-        return;
-    }
 
     for (var id in usertags) {
-        if (usertags[id] === socket.handshake.query.tag) {
+        if (socket.handshake.query.tag.length > 0 && usertags[id] === socket.handshake.query.tag) {
             socket.disconnect();
             return;
         }
